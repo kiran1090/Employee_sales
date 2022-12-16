e68b2b422de50f4757217dc520f9d52db97dd590
kiran_a <iamkiranashok@gmail.com>
2022-12-16 10:43:36 +0530

    first commit

---
+++ src/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+    private String name;
+    private double salary;
+    private int age;
+    public Employee(String name,double salary,int age)
+    {
+        this.name=name;
+        this.salary=salary;
+        this.age=age;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public double getSalary()
+    {
+        return this.salary;
+    }
+    public int getAge()
+    {
+        return this.age;
+    }
+    public void raiseSalary()
+    {
+        this.salary=this.salary + 2000;
+    }
+}
